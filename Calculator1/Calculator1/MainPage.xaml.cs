8ea0d889ba9eef93bef3405db4e869cf78536b4d
Brayan131 <brayan@live.at>
2018-08-13 09:03:06 +0200

    made version 1 of calulator.
    number buttons are not implemented.
    
    will change the interface

--- Calculator1/Calculator1/MainPage.xaml.cs
+++ Calculator1/Calculator1/MainPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Calculator1.ViewModels;
 using Xamarin.Forms;
 
 namespace Calculator1
@@ -12,6 +13,7 @@ namespace Calculator1
 		public MainPage()
 		{
 			InitializeComponent();
+		    BindingContext = new MainPageViewModel();
 		}
 	}
 }
---
+++ Calculator1/Calculator1/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Calculator1.ViewModels
+{
+    class MainPageViewModel : BaseViewModel
+    {
+        private string entry_Number1;
+        private string entry_Number2;
+        private string label_Operation;
+        private string label_Result;
+
+        private char _operation;
+
+        public ICommand Button_Plus { get; private set; }
+        public ICommand Button_Minus { get; private set; }
+        public  ICommand Button_Divid { get; private set; }
+        public ICommand Button_Multiply { get; private set; }
+
+
+        public MainPageViewModel()
+        {
+            Button_Plus = new Command(x =>
+            {
+                _operation = '+';
+                base.OnPropertyChanged(nameof(Label_Operation));
+                base.OnPropertyChanged(nameof(Label_Result));
+            });
+            Button_Minus = new Command(x =>
+            {
+                _operation = '-';
+                base.OnPropertyChanged(nameof(Label_Operation));
+                base.OnPropertyChanged(nameof(Label_Result));
+            });
+            Button_Divid = new Command(x =>
+            {
+                _operation = '/';
+                base.OnPropertyChanged(nameof(Label_Operation));
+                base.OnPropertyChanged(nameof(Label_Result));
+            });
+            Button_Multiply = new Command(x =>
+            {
+                _operation = '*';
+                base.OnPropertyChanged(nameof(Label_Operation));
+                base.OnPropertyChanged(nameof(Label_Result));
+            });
+        }
+
+        public string Entry_Number1
+        {
+            get => entry_Number1;
+            set
+            {
+                if (entry_Number1 != value) entry_Number1 = value;
+                else return;
+
+                base.OnPropertyChanged();
+                base.OnPropertyChanged(nameof(Label_Result));
+            }
+        }
+
+        public string Entry_Number2
+        {
+            get => entry_Number2;
+            set
+            {
+                if (entry_Number2 != value) entry_Number2 = value;
+                else return;
+
+                base.OnPropertyChanged();
+                base.OnPropertyChanged(nameof(Label_Result));
+            }
+        }
+
+        
+        public string Label_Operation
+        {
+            get => _operation.ToString();
+            set
+            {
+                if (label_Operation != value) label_Operation = value;
+                else return;
+
+                base.OnPropertyChanged();
+            }
+        }
+        
+        public string Label_Result
+        {
+            get
+            {
+                if (entry_Number1 != null && entry_Number2 != null)
+                {
+                    if (_operation.Equals('+')) return (double.Parse(entry_Number1) + double.Parse(entry_Number2)).ToString();
+                    else if (_operation.Equals('-')) return (double.Parse(entry_Number1) - double.Parse(entry_Number2)).ToString();
+                    else if (_operation.Equals('/')) return (double.Parse(entry_Number1) / double.Parse(entry_Number2)).ToString();
+                    else if (_operation.Equals('*')) return (double.Parse(entry_Number1) * double.Parse(entry_Number2)).ToString();
+                }
+                return "0";
+            }
+            set
+            {
+                if (label_Result != value) label_Result = value;
+                else return;
+
+                base.OnPropertyChanged();
+            }
+        }
+    }
+}
